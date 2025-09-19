deda9df8b871dfa4f4cc69987473345abd6608e6
Justin Chen <jchen4086@gmail.com>
2025-09-19 16:55:23 -0400

    piano keys connection poc scene

--- wind-tempo/Test.gd
+++ wind-tempo/Test.gd
@@ -1,16 +1,16 @@
 extends Node
 
 func _ready():
-	OS.open_midi_inputs()
-	print(OS.get_connected_midi_inputs())
+  OS.open_midi_inputs()
+  print(OS.get_connected_midi_inputs())
 
-func _input(event);
-	if event is InputEventMIDI:
-		print("MIDI button pressed.")
-		printt("Channel", event.channel)
-		printt("Pitch", event.pitch)
-		printt("Velocity", event.velocity)
-		printt("Message", event.message)
-		printt("Instrument", event.instrument)
-		printt("Pressure", event.pressure)
-		printt("-=-=-=-=-=-=-=-=-=-=-")
+func _input(event):
+  if event is InputEventMIDI:
+    print("MIDI button pressed.")
+    printt("Channel", event.channel)
+    printt("Pitch", event.pitch)
+    printt("Velocity", event.velocity)
+    printt("Message", event.message)
+    printt("Instrument", event.instrument)
+    printt("Pressure", event.pressure)
+    printt("-=-=-=-=-=-=-=-=-=-=-")
