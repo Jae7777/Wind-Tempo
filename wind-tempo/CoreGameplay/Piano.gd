1a86c6f39efdf076e1f322ec834ecc74ac7afae6
WangWNico <nicowang0404@gmail.com>
2025-12-05 16:13:51 -0500

    Created Hold Note scene and updated piano

--- wind-tempo/CoreGameplay/Piano.gd
+++ wind-tempo/CoreGameplay/Piano.gd
@@ -1,6 +1,7 @@
 extends Node
 
 signal key_pressed(lane: int)
+signal key_released(lane: int)
 
 @export var lane_actions: Array = ["lane_0", "lane_1", "lane_2", "lane_3"]
 
@@ -9,3 +10,5 @@ func _process(_delta: float) -> void:
 	for i in range(lane_actions.size()):
 		if Input.is_action_just_pressed(lane_actions[i]):
 			emit_signal("key_pressed", i)
+		if Input.is_action_just_released(lane_actions[i]):
+			emit_signal("key_released", i)
