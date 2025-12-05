24acb99be7b5100e55b10e7066fddc6b19f13a27
WangWNico <nicowang0404@gmail.com>
2025-12-05 16:06:30 -0500

    Main Menu

---
+++ wind-tempo/MainMenu.gd
@@ -0,0 +1,15 @@
+extends Control
+
+@onready var play_button = $VBoxContainer/PlayButton
+@onready var quit_button = $VBoxContainer/QuitButton
+
+func _ready() -> void:
+	play_button.connect("pressed", Callable(self, "_on_play_pressed"))
+	quit_button.connect("pressed", Callable(self, "_on_quit_pressed"))
+
+func _on_play_pressed() -> void:
+	# Load the gameplay scene
+	get_tree().change_scene_to_file("res://CoreGameplay/Main.tscn")
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
